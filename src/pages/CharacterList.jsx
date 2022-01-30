6ec4fbacd3a4c88ff8fd68b2325d0d2c5dfd305c
Jose De La Cruz <jose.delcruz0490@icloud.com>
2022-01-30 10:23:26 -0500

    Added notes to CharacterList

--- src/pages/CharacterList.jsx
+++ src/pages/CharacterList.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { useQuery, gql } from '@apollo/client';
 import './CharacterList.css';
 
+// The useQuery React hook is the primary API for executing queries in an Apollo application. We run a query within a React component by calling useQuery and passing it out GraphQL query string.
+
+// When our component renders, useQuery returns an object from Apollo Client that contains loading, error and data properties that we can use to render our UI.
+
 const GET_CHARACTERS = gql`
 	query {
 		characters {
